b6f61c79fd50e512b6e5a8271bd657c1d2ae151d
Chaitali <chaitalipatel18j@gmail.com>
2016-04-22 16:42:14 +1200

    Add user and styles

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module RecipeBox
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #needed for devise
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   end
 end
